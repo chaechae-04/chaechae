9b0881ddd2b16cc4873b7f2a9be591358ef97782
lee0su <leeys4903@naver.com>
2024-12-15 01:16:05 +0900

    20241215 01:16

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link'
+
 export default function Header() {
     return (
-        <header className="min-h-screen bg-[#051c2c]">
-            <nav className="max-w-7xl max-auto px-4 py-4 flex items-center justify-between border-b border-white">
-                <div className="ml-10 text-white text-2xl font-bold">채채의 개발로그</div>
-                <div className="flex gap-4 text-gray-300">
-                    <a href="/posts" className="hover:text-white">글</a>
-                    <a href="/tags" className="hover:text-white">태그</a>
-                    <a href="/about" className="hover:text-white">소개</a>
+            <nav className="mx-auto max-auto px-10 py-4 flex flex-col">
+                <div className="flex items-center justify-between">
+                    <div className="ml-10 text-white text-2xl font-bold">채채의 개발로그</div>
+                    <div className="flex gap-4 text-gray-300 pr-10">
+                        <Link href="/posts" className="hover:text-white px-2">글</Link>
+                        <Link href="/tags" className="hover:text-white px-2">태그</Link>
+                        <Link href="/about" className="hover:text-white px-2">소개</Link>
+                    </div>
                 </div>
+                <div style={{ width: '100% '}} className="bg-gray-400 h-px mt-2 mx-auto mt-5" />
             </nav>
-        </header>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default async function Home() {
   return (
     <div className="min-h-screen bg-[#051c2c]">
       <Header />
+      <div className="flex jusity-center mt-4">
+        <img src="/imgs/temp.jpg" alt="임시 이미지" className="w-full amx-w-4xl rounded-lg" />
+      </div>
       <main className="max-w-7xl mx-auto px-4 py-8">
         <div className="grid gap-6">
           {posts.map((post) => (
