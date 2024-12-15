be07475122b3d9f049ab808120f18bb6a4ad94e6
lee0su <leeys4903@naver.com>
2024-12-15 22:34:11 +0900

    add : aside content

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,28 @@ export default async function Home() {
         <img src="/imgs/temp.jpg" alt="임시 이미지" className="w-full md:w-3/4 lg:w-1/2 rounded-lg mx-auto" />
       </div>
       <TabBar />
-      <main className="max-w-7xl mx-auto px-4 py-8">
-        <div className="grid gap-6">
-          {posts.map((post) => (
-            <article key={post.id} className="bg-[#0A2540] p-4 md:p-6 lg:p-8 rounded-lg hover:bg-[#123456] transition-colors">
-              <Link href={`/posts/${post.id}`}>
-                <h2 className="text-white text-lg md:text-xl lg:text-2xl font-bold mb-2">{post.title}</h2>
-                <p className="text-gray-400 text-sm md:text-base mb-4">{post.excerpt}</p>
-                <div className="text-xs md:text-sm text-gray-500">
-                  <span>{post.date}</span>
-                </div>
-              </Link>
-            </article>
-          ))}
+      <div className="max-w-7xl mx-auto flex px-4 py-8 space-x-4">
+        <div className="w-3/4 flex flex-col">
+          <main className="flex-1">
+            <div className="grid gap-6">
+              {posts.map((post) => (
+                <article key={post.id} className="bg-[#0A2540] p-4 md:p-6 lg:p-8 rounded-lg hover:bg-[#123456] transition-colors">
+                  <Link href={`/posts/${post.id}`}>
+                    <h2 className="text-white text-lg md:text-xl lg:text-2xl font-bold mb-2">{post.title}</h2>
+                    <p className="text-gray-400 text-sm md:text-base mb-4">{post.excerpt}</p>
+                    <div className="text-xs md:text-sm text-gray-500">
+                      <span>{post.date}</span>
+                    </div>
+                  </Link>
+                </article>
+              ))}
+            </div>
+          </main>
         </div>
-      </main>
+        <aside className="w-1/4 bg-[#0A2540] p-4 rounded-lg">
+              <h3 className="text-white text-lg font-bold md-4">추가 정보</h3>
+        </aside>
+      </div>
     </div>
   )
 }
