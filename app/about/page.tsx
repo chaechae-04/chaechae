7f0430c250c2bd03226569cd8a29030721f971d9
lee0su <leeys4903@naver.com>
2024-12-16 00:28:32 +0900

    add : About-Education

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,11 +5,24 @@ export default function About() {
     return (
         <div className="min-h-screen bg-[#051c2c]">
             <Header />
-            <div className="text-white flex flex-col items-center justify-center p-8">
-                <h1 className="text-3xl font-bold mb-4">About</h1>
-                <p className="text-lg text-gray-400">
-                    자세한 내용
+            <div className="bg-[#051c2c] text-white flex flex-col items-center justify-center mx-auto md:mt-[50px] lg:mt-[100px] xl:mt-[150px]">
+                <p className="text-xxs xl:text-4xl font-bold mb-6">
+                    안녕하세요, 저는 [이름]입니다.
+                    </p>
+                <p className="text-xxxs xl:text-lg text-gray-400 font-bold mb-4">
+                    백엔드, 게임 개발자로, 항상 새로운것을 알아가는것에 열정을 가지고 있습니다.
                 </p>
+                
+                <div style={{ width: '70% '}} className="bg-gray-400 h-px mt-2 mx-auto mt-5 mb-10" />
+
+                <div className="flex flex-col w-[65%] mx-auto">
+                    <p className="flex text-xxxs xl:text-2xl font-bold mb-3 text-left">
+                        경력
+                    </p>
+                    <ul className="list-disc list-inside text-gray-300">
+                        <li>[국립한국교통대학교] - 컴퓨터 공학 전공</li>
+                    </ul>
+                </div>
             </div>
             <Footer />
         </div>
