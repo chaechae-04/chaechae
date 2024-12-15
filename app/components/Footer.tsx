6a6e242e503bc5e0def1ade71991ae20f7800863
lee0su <leeys4903@naver.com>
2024-12-15 23:42:52 +0900

    update : Footer Font-Size _2

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer className="bg-[#051c2c] text-gray-400 text-center py-4">
             <div className="items-center">
-                <p className="text-xxs xl:text-xs mb-2">
+                <p className="text-xxxs xl:text-xs mb-2">
                     © 2024 chaechae. 모든 권리 보유. | 이 페이지는 내 개발 일지를 기록하는 테크로그입니다.
                 </p>
                 <div className="flex flex-row text-xs font-bold space-w-4 justify-center">
