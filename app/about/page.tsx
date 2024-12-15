f69f4d8879a0e84b821831c254c7b5fd25afcdec
lee0su <leeys4903@naver.com>
2024-12-16 01:57:40 +0900

    add : About Contents

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,17 @@
 import Header from '@/app/components/Header'
 import Footer from '@/app/components/Footer'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+import Link from 'next/link'
+
 export default function About() {
     return (
         <div className="min-h-screen bg-[#051c2c]">
             <Header />
-            <div className="bg-[#051c2c] text-white flex flex-col items-center justify-center mx-auto md:mt-[50px] lg:mt-[100px] xl:mt-[150px]">
+            <div className="bg-[#051c2c] text-white flex flex-col items-center justify-center mx-auto md:my-[50px] lg:my-[100px] xl:my-[150px]">
                 <p className="text-base xl:text-4xl font-bold mb-6">
                     안녕하세요, 저는 [chaechae-04] 입니다.
                     </p>
@@ -15,13 +21,77 @@ export default function About() {
                 
                 <div style={{ width: '55% '}} className="bg-gray-600 h-px mt-2 mx-auto mt-5 mb-10" />
 
-                <div className="flex flex-col w-[50%] mx-auto">
-                    <p className="flex text-xs xl:text-2xl font-bold mb-3 text-left">
-                        경력
-                    </p>
-                    <ul className="list-disc list-inside text-gray-300 text-xxs xl:text-xl">
-                        <li>[국립한국교통대학교] - 컴퓨터 공학 전공</li>
-                    </ul>
+                <div className="flex flex-col w-[50%] mx-auto space-y-10">
+                    <div>
+                        <p className="flex text-xs xl:text-2xl font-bold mb-3 text-left">
+                            PERSONAL HISTORY
+                        </p>
+                        <ul className="list-disc list-inside text-gray-300 text-xxs xl:text-xl">
+                            <li>[국립한국교통대학교] - 컴퓨터 공학 전공</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className="flex text-xs xl:text-2xl font-bold mb-3 text-left">
+                            PROJECTS
+                        </p>
+                        <ul className="list-disc list-inside text-gray-300 text-xxs xl:text-xl">
+                            <li> <Link href="https://github.com/chaechae-04/LiquorLounge" target="_blank" className="underline">LiquorLounge</Link> : Web Application </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className="flex text-xs xl:text-2xl font-bold mb-3 text-left">
+                            STACKS
+                        </p>
+                        <div className="flex flex-col mx-auto ml-5 space-y-2">
+                            <div className="flex flex-row space-x-2">
+                                <img src="https://img.shields.io/badge/Java-007396.svg?&style=for-the-badge&logo=openjdk&logoColor=white" />
+                                <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white" />
+                                <img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white" />
+                                <img src="https://img.shields.io/badge/swift-F54A2A?style=for-the-badge&logo=swift&logoColor=white" />
+                            </div>
+                            <div className="flex flex-row space-x-2">
+                                <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+                                <img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+                                <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+                            </div>
+                            <div className="flex flex-row space-x-2">
+                                <img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black" />
+                                <img src="https://img.shields.io/badge/node.js-339933?style=for-the-badge&logo=Node.js&logoColor=white" />
+                                <img src="https://img.shields.io/badge/springboot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white" />
+                                <img src="https://img.shields.io/badge/mysql-4479A1?style=for-the-badge&logo=mysql&logoColor=white" />
+                            </div>
+                            <div className="flex flex-row space-x-2">
+                                <img src="https://img.shields.io/badge/unity-%23000000.svg?style=for-the-badge&logo=unity&logoColor=white" />
+                                <img src="https://img.shields.io/badge/git-F05032?style=for-the-badge&logo=git&logoColor=white" />
+                                <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white" />
+                            </div>
+                        </div>                         
+                    </div>
+                    
+                    <div>
+                        <p className="flex text-xs xl:text-2xl font-bold mb-3 text-left">
+                            Contact
+                        </p>
+                        <div className="flex flex-col mx-auto ml-5 space-y-2">
+                            <div className="flex flex-row space-x-2">
+                                <Link href="https://github.com/chaechae-04" className="flex text-gray-300 hover:text-white mx-1 space-x-2" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faGithub} className="w-4 h-4 mr-1" /> GitHub : <Link href="https://github.com/chaechae-04" target="_blank">chaechae-04</Link>
+                                </Link>
+                            </div>
+                            <div className="flex flex-row space-x-2">
+                                <Link href="https://instagram.com/lee.young_s" className="flex text-gray-300 hover:text-white mx-1 space-x-2" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faInstagram} className="w-4 h-4 mr-1" /> Instagram : <Link href="https://instagram.com/lee.young_s" target="_blank">lee.young_s</Link>
+                                </Link>
+                            </div>
+                            <div className="flex flex-row space-x-2">
+                                <Link href="mailto:leeys4903@naver.com" className="flex text-gray-300 hover:text-white mx-1 space-x-2">
+                                    <FontAwesomeIcon icon={faEnvelope} className="w-4 h-4 mr-1" /> Email <Link href="mailto:leeys4903@naver.com" className='underline'>leeys4903@naver.com</Link>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
             <nav className="mx-auto max-w-screen-xl px-4 sm:px-10 py-4 flex flex-col">
                 <div className="flex items-center justify-between">
-                    <div className="ml-0 sm:ml-10 text-white text-xl sm:text-2xl font-bold">채채의 개발로그</div>
+                    <Link href="/" className="ml-0 sm:ml-10 text-white text-xl sm:text-2xl font-bold">채채의 개발로그</Link>
                     <div className="flex gap-4 text-gray-300 pr-4 sm:pr-10">
                         <Link href="/posts" className="hover:text-white px-2 text-sm sm:text-base">Posts</Link>
                         <Link href="/tags" className="hover:text-white px-2 text-sm sm:text-base">Tags</Link>
