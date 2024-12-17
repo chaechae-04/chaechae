76c24b9360c11b613890f405aff4d1996226fdb0
lee0su <leeys4903@naver.com>
2024-12-18 04:06:36 +0900

    fix : About HTML Badge Mobile View CSS fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -132,7 +132,7 @@ export default function About() {
                             <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
                             <img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
                             <img src="https://img.shields.io/badge/swift-F54A2A?style=for-the-badge&logo=swift&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
-                            <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white" className="rounded-2x hover:opacity-80 transition-opacity duration-500"/>
+                            <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500"/>
                             <img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
                             <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
                             <img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white" className="rounded-2xl hover:opacity-80 transition-opacity duration-500" />
