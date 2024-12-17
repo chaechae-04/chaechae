5937e2c7e518791e5680e79b1a97df0dbb2c86f6
lee0su <leeys4903@naver.com>
2024-12-18 05:32:27 +0900

    remove : tailwind.config.ts Animation: fadeIn, fadeOut

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,20 +12,6 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '0.7' },
-        },
-        fadeOut: {
-          '0%': { opacity: '0.7' },
-          '100%': { opacity: '0' },
-        },
-      },
-      animation: {
-        fadeIn: 'fadeIn 0.5s ease-in forwards',
-        fadeOut: 'fadeOut 0.5s ease-out forwards',
-      },
     },
   },
   plugins: [],
