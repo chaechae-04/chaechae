6a2b2b6ef0934556cba6918b20b725b6f8908cba
lee0su <leeys4903@naver.com>
2024-12-18 18:45:22 +0900

    update : globals.css .prose pre margin-bottom

--- app/components/MarkdownRender.tsx
+++ app/components/MarkdownRender.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { MarkdownProps } from '@/types/post'
@@ -9,6 +10,7 @@ const MarkdownRenderer = ({ content }: MarkdownProps ) => {
         <div className="m-5">
             <ReactMarkdown
                 remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw]}
                 components={{
                     code({ children }) {
                         return <SyntaxHighlighter>{children}</SyntaxHighlighter>
