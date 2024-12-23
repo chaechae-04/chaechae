2c706d7753bd78ffd3c0b2fea00f218979714ba5
lee0su <leeys4903@naver.com>
2024-12-24 00:01:31 +0900

    fix CatCursor.tsx : cat speed error

--- app/components/CatCursor.tsx
+++ app/components/CatCursor.tsx
@@ -19,8 +19,8 @@ const CatCursor: React.FC = () => {
                 const dy = newMousePosition.y - prev.y 
                 const distance = Math.sqrt(dx * dx + dy * dy) 
 
-                const moveX = (dx / distance) * speed 
-                const moveY = (dy / distance) * speed 
+                const moveX = (dx / distance) 
+                const moveY = (dy / distance)
 
                 if (distance > 0) {
                     if (distance > speed) {
