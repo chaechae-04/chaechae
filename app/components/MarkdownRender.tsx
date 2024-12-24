cbe9653be39856926ac6c651ee002aa5e7889912
lee0su <leeys4903@naver.com>
2024-12-24 18:09:08 +0900

    fix MarkdownRender.tsx : img size

--- app/components/MarkdownRender.tsx
+++ app/components/MarkdownRender.tsx
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 
+import Image from 'next/image'
+
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { MarkdownProps } from '@/types/post'
 
@@ -14,6 +16,18 @@ const MarkdownRenderer = ({ content }: MarkdownProps ) => {
                 components={{
                     code({ children }) {
                         return <SyntaxHighlighter>{children}</SyntaxHighlighter>
+                    },
+                    img({ ...props }) {
+                        return (
+                            <Image
+                                {...props}
+                                width={1200}
+                                height={800}
+                                className="w-full md:w-[75%] lg:w-[50%] xl:w-[50%] object-contain"
+                                alt={props.alt || ''}
+                                priority
+                            />
+                        )
                     }
                 }} className="text-xs md:text-base lg:text-lg xl:text-lg">
                 {content}
