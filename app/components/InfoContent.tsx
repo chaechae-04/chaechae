349be6d1684365c1fad1579b878fd61ce059dd92
lee0su <leeys4903@naver.com>
2025-01-18 22:06:06 +0900

    update content : InfoContent.tsx

--- app/components/InfoContent.tsx
+++ app/components/InfoContent.tsx
@@ -2,7 +2,8 @@ export default function InfoContent() {
     return (
         // <h3 className="text-white text-lg font-bold md-4">추가 정보</h3>
         <ul className="text-white text-lg font-bold md-4 px-4">
-            <li className="py-4">More Info</li>
+            <li className="py-4">programmers 과제 테스트</li>
+            <li className="py-4">API 요청 및 반응</li>
         </ul>
     )
 }
